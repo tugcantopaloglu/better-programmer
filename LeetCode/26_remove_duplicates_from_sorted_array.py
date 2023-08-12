9a53c4d4f57182f0811eb43e925488fca6c25833
Tuğcan Topaloğlu <53763911+tugcantopaloglu@users.noreply.github.com>
2023-08-12 14:32:42 +0300

    3q

--- LeetCode/26_remove_duplicates_from_sorted_array.py
+++ LeetCode/26_remove_duplicates_from_sorted_array.py
@@ -1,1 +1,13 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        seen = []
+        ln = len(nums)-1
+        while ln > -1:
+            if nums[ln] in seen:
+                nums.remove(nums[ln])
+            else:
+                seen.append(nums[ln])
+            ln -= 1
+        return len(nums)
 
+# SEEMS LIKE 3000MS BAD RUNTIME!
--- LeetCode/27_remove_element.py
+++ LeetCode/27_remove_element.py
@@ -1,1 +1,8 @@
-
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        metric = len(nums)-1
+        while metric > -1:
+            if nums[metric] == val:
+                del nums[metric]
+            metric -= 1
+        return len(nums)
